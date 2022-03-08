91f97915a7feac7bea2e6545647e2d3c0f596112
ZhaoMeap <bill56523@gmail.com>
2022-03-08 11:12:50 +0800

    My First Commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,13 @@
+import 'dart:ui';
 import 'package:flutter/material.dart';
 
 void main() {
   var appTitle = Text('我的第一個Flutter App'),
       hiFlutter = Text(
-      'Hi, Flutter.',
-        style: TextStyle(fontSize: 50),
+      'Hi, Flutter.\n'
+      '今天是20220308',
+        style: TextStyle(fontSize: 30,fontWeight: FontWeight.bold, color: Colors.red
+            , decoration: TextDecoration.lineThrough),
       );
 
   //建立appBody物件
@@ -14,12 +17,14 @@ void main() {
   //建立appBar物件
   var appBar = AppBar(
     title: appTitle,
+    backgroundColor: Colors.red,
   );
   //建立app物件
   var app = MaterialApp(
     home: Scaffold(
       appBar: appBar,
       body: appBody,
+      backgroundColor: Colors.deepOrange[200],
     ),
   );
   runApp(app);
